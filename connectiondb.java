4cffc920a75a770166f79b430a3dd187d9e76b49
abirhassan221-15-5121 <124512940+abirhassan221-15-5121@users.noreply.github.com>
2023-06-24 01:59:46 -0700

    Add files via upload
    BloodBankManagement is a console based application using java. In this project I used java all oop concept from object create to file handlin.Donner database is created using file.

---
+++ connectiondb.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bloodbankmanagements;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author wahid
+ */
+public class connectiondb {
+
+  Connection con = null;
+    public static Connection connectDB(){
+        try {           
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con  = DriverManager.getConnection("jdbc:mysql://localhost/Diu_projects","wahid","");
+            JOptionPane.showMessageDialog(null,"connected");
+            return con;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Database connection failed " );
+            return null;
+    }  
+  }
+}
